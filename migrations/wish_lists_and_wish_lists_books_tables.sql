9d7316f277433696b8117b159b3c7c7e1ddd4d76
lamad019 <lamad019@fiu.edu>
2023-06-06 18:13:21 -0700

    added wish lists and wish lists books tables

---
+++ migrations/wish_lists_and_wish_lists_books_tables.sql
@@ -0,0 +1,17 @@
+use geek_text;
+
+create table wish_lists (
+id int AUTO_INCREMENT PRIMARY KEY,
+user_id int,
+FOREIGN KEY (user_id) references users(id)
+);
+
+create table wish_lists_books (
+id int AUTO_INCREMENT PRIMARY KEY,
+wish_list_id int,
+book_id int,
+created_at date,
+deleted_at date,
+FOREIGN KEY (wish_list_id) REFERENCES wish_lists(id),
+FOREIGN KEY (book_id) REFERENCES books(id)
+);
