b2cb488641393865745107bae9213cde487e42b9
lamad019 <98285340+lamad019@users.noreply.github.com>
2023-06-06 18:18:09 -0700

    Update migrations/wish_lists_and_wish_lists_books_tables.sql
    Co-authored-by: Eric Andrews <eandrews737@gmail.com>

--- migrations/wish_lists_and_wish_lists_books_tables.sql
+++ migrations/wish_lists_and_wish_lists_books_tables.sql
@@ -7,11 +7,11 @@ FOREIGN KEY (user_id) references users(id)
 );
 
 create table wish_lists_books (
-id int AUTO_INCREMENT PRIMARY KEY,
-wish_list_id int,
-book_id int,
-created_at date,
-deleted_at date,
-FOREIGN KEY (wish_list_id) REFERENCES wish_lists(id),
-FOREIGN KEY (book_id) REFERENCES books(id)
+  id int AUTO_INCREMENT PRIMARY KEY,
+  wish_list_id int,
+  book_id int,
+  created_at date,
+  deleted_at date,
+  FOREIGN KEY (wish_list_id) REFERENCES wish_lists(id),
+  FOREIGN KEY (book_id) REFERENCES books(id)
 );
