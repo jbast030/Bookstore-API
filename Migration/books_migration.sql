5be78166669a5164c5048e33e754fe741109fbe2
Nicholas Belschner <101606770+NicholasBelschner@users.noreply.github.com>
2023-06-08 19:59:58 -0400

    Update Migration/books_migration.sql
    Co-authored-by: Eric Andrews <eandrews737@gmail.com>

--- Migration/books_migration.sql
+++ Migration/books_migration.sql
@@ -3,6 +3,6 @@ use geek_text;
 CREATE TABLE books (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name TINYTEXT,
-  created_at VARCHAR(50),
-  deleted_at VARCHAR(50)
+  created_at DATE,
+  deleted_at DATE
 );
