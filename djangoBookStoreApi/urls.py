6a17f2a5cc7d48a60e0e3a1007fe4d9b917e2873
lamad019 <lamad019@fiu.edu>
2023-07-09 10:48:43 -0700

    WISH-2: Create New Wishlist Post Request made.

--- djangoBookStoreApi/urls.py
+++ djangoBookStoreApi/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views.test_views import test_view
 from .views.shopping_carts_view import shopping_carts_view
 from .views.user_profile_view import UserProfileView
+
 from .views.wish_list_books_view import wish_list_books_view
 from .views.wish_list_books_view import add_book_to_wishlist
-
+from .views.wish_list_books_view import create_new_wishlist
 # add route paths here
 urlpatterns = [
     path('test/', test_view),
@@ -12,4 +13,5 @@ urlpatterns = [
     path('user/<int:user_id>/profile', UserProfileView.as_view(), name='user_profile'),
     path('user/<int:id>/wishlist', wish_list_books_view),
     path('user/wishlist/add_book', add_book_to_wishlist),
+    path('user/wishlist/create_wishlist', create_new_wishlist),
 ]
--- djangoBookStoreApi/views/wish_list_books_view.py
+++ djangoBookStoreApi/views/wish_list_books_view.py
@@ -5,6 +5,7 @@ import json
 from ..models.WishLists import WishLists
 from ..models.WishListBooks import WishListBooks
 from ..models.Books import Books
+from ..models.Users import Users
 
 #Get Request via WishList Id
 def wish_list_books_view(request, id):
@@ -18,6 +19,7 @@ def wish_list_books_view(request, id):
         })
     return JsonResponse(book_list, safe=False)
 
+#Post Request - Create new book entry inside wish_list_books table via existing Book ID and Wishlist ID
 def add_book_to_wishlist(request):
     #Generate queryset of all book and wishlists.
     bookset = Books.objects.all()
@@ -70,8 +72,75 @@ def add_book_to_wishlist(request):
         )
 
         response = {
-            'Message' : 'Book ID has been successfully added to associated Wishlist.'
-            
+            'Message' : 'Book ID has been successfully added to associated Wishlist.',
+            'Book ID' : bookID,
+            'Wish List ID' : wishlistID
         }
         return JsonResponse(response, safe=False)
     
+def create_new_wishlist(request):
+    #Generate two sets for checks. One for user IDs, the other for available names.
+    userSet = Users.objects.all()
+    wishlistSet = WishLists.objects.all()
+
+    #Lists to be filled out with their associated info.
+    userID_list = []
+    for uID in userSet:
+        userID_list.append(uID.id)
+
+    wishlistName_List = []
+    for wID in wishlistSet:
+        wishlistName_List.append(wID.wishlist_name)
+
+    #Obtain user ID and their wishlist name.
+    try:
+        data = json.loads(request.body)
+    except json.JSONDecodeError:
+        data = {}
+    
+    #Place the ID and wishlist into their associated variables.
+    userID = data.get('user_id')
+    wishlistName = data.get('wishlist_name')
+
+    #Check whether userID or wishlist name is missing. If so, send error. If not, continue to next check.
+    if userID == None or wishlistName == None:
+        response = {
+            'Error' : 'User ID or Wishlist Name is missing. Please enter it again.'
+        }
+        return JsonResponse(response, safe=False)
+
+    #Check if the associated user has more than 3 wishlists. If so, send error. Else, continue to next check.
+    userCount = WishLists.objects.filter(user_id=userID).count()
+    if userCount >= 3:
+        response = {
+            'Error' : 'The user has reached the max number of wish lists (3).'
+        }
+        return JsonResponse(response, safe=False)
+
+    #Check if userID exists in the database. If not, send error. Else, continue to next check.
+    if userID not in userID_list:
+        response = {
+            'Error' : 'User ID does not exist within the database.'
+        }
+        return JsonResponse(response, safe=False)
+    
+    #Check if wishlist name is available. If not, send error. Else, continue to final part.
+    if wishlistName in wishlistName_List:
+        response = {
+            'Error' : 'Wishlist Name already exists within the database. Please enter a different name.'
+        }
+        return JsonResponse(response, safe=False)
+    else:
+        #Create new wishlist with user ID and wishlist name.
+        WishLists.objects.create(
+            user_id = userID,
+            wishlist_name = wishlistName
+        )
+
+        response = {
+            'Message' : 'New wishlist created.',
+            'User ID' : userID,
+            'Wish List Name' : wishlistName
+        }
+
+        return JsonResponse(response, safe=False)
